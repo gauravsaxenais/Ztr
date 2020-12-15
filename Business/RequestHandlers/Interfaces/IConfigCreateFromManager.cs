eac0b5592a5acef88bae3f02aaa4ced3fba7a4b9
gsaxena <gaurav.saxena@rsystems.com>
2020-12-15 15:22:04 +0530

    fixed issues with modulemanager.

---
+++ Business/RequestHandlers/Interfaces/IConfigCreateFromManager.cs
@@ -0,0 +1,20 @@
+﻿namespace Business.RequestHandlers.Interfaces
+{
+    using Business.Models;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// This module takes config.toml as input 
+    /// and generates a model for the config.toml.
+    /// </summary>
+    public interface IConfigCreateFromManager
+    {
+        /// <summary>
+        /// Gets the default values all modules asynchronous.
+        /// </summary>
+        /// <param name="configTomlFile">The firmware version.</param>
+        /// <returns></returns>
+        Task<IEnumerable<ModuleReadModel>> GenerateConfigTomlModelAsync(string configTomlFile);
+    }
+}
---
+++ DeviceConfigAPI/Controllers/ConfigCreateFromController.cs
@@ -0,0 +1,54 @@
+﻿namespace Service.Controllers
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+    using Business.Models;
+    using Business.RequestHandlers.Interfaces;
+    using EnsureThat;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using ZTR.Framework.Service;
+
+    /// <summary>
+    /// This service returns all the modules and their default values.
+    /// If any module doesnt have any default values, then
+    /// only the attributes are returned.
+    /// </summary>
+    /// <seealso cref="ControllerBase" />
+    [Produces(SupportedContentTypes.Json, SupportedContentTypes.Xml)]
+    [Consumes(SupportedContentTypes.Json, SupportedContentTypes.Xml)]
+    [QueryRoute]
+    public class ConfigCreateFromController : ApiControllerBase
+    {
+        private readonly IDefaultValueManager manager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigCreateFromController"/> class.
+        /// </summary>
+        /// <param name="manager">interface of the 'backend' manager which does all the work.</param>
+        public ConfigCreateFromController(IDefaultValueManager manager)
+        {
+            EnsureArg.IsNotNull(manager, nameof(manager));
+
+            this.manager = manager;
+        }
+
+        /// <summary>
+        /// Gets default values for all the modules.
+        /// </summary>
+        /// <returns>A <see cref="IEnumerable{ModuleReadModel}"/> representing the result of the operation.</returns>
+        /// <param name="firmwareVersion">firmware version.</param>
+        /// <param name="deviceType">device type.</param>
+        [HttpGet(nameof(GetAllDefaultValues))]
+        [ProducesResponseType(typeof(IEnumerable<ModuleReadModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAllDefaultValues([Required, FromQuery] string firmwareVersion, [Required, FromQuery] string deviceType)
+        {
+            var result = await this.manager.GetDefaultValuesAllModulesAsync(firmwareVersion, deviceType).ConfigureAwait(false);
+
+            return this.StatusCode(StatusCodes.Status200OK, result);
+        }
+    }
+}
