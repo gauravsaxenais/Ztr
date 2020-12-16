e21eb03b80a20b875bd331fc4ff525c6e57c14af
gsaxena <gaurav.saxena@rsystems.com>
2020-12-16 12:22:02 +0530

    changes

---
+++ Business/RequestHandlers/Managers/ConfigCreateFromManager.cs
@@ -0,0 +1,19 @@
+﻿namespace Business.RequestHandlers.Managers
+{
+    using Business.RequestHandlers.Interfaces;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
+    using ZTR.Framework.Business;
+
+    public class ConfigCreateFromManager : Manager, IConfigCreateFromManager
+    {
+        /// <summary>Initializes a new instance of the <see cref="ConfigCreateFromManager" /> class.</summary>
+        /// <param name="logger">The logger.</param>
+        public ConfigCreateFromManager(ILogger<ConfigCreateFromManager> logger) : base(logger) { }
+
+        public async Task GenerateConfigTomlModelAsync(string configTomlFile)
+        {
+            
+        }
+    }
+}
