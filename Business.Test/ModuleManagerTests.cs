73fee01b7f0080df5b11cb1321195b1da6a5623a
gsaxena <gaurav.saxena@rsystems.com>
2021-01-17 17:48:44 +0530

    minor warning removed.

--- Business.Test/ModuleManagerTests.cs
+++ Business.Test/ModuleManagerTests.cs
@@ -1,19 +1,15 @@
 ﻿namespace Business.Test
 {
-    using Business.GitRepositoryWrappers.Interfaces;
-    using Moq;
     using Xunit;
 
     // naming convention - [MethodWeTest_StateUnderTest_ExpectedBehavior]
 
     public class ModuleManagerTests
     {
-        private readonly IModuleServiceManager _moduleServiceManager;
         [Fact]
         public void GetAllModulesAsync_AllModules_ReturnsAllModules()
         {
             // Arrange
-            var mockServiceManager = new Mock<IModuleServiceManager>();
             //mockServiceManager.Setup(x => x.GetAllModulesAsync("1.0.21", "m7"))
             //    .Returns((Task<List<ModuleReadModel>>) => new List<ModuleReadModel>());
 
